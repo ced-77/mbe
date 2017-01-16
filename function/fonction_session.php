058b496092ae08ac86632eb81becd010942a39d7
ced-77 <ced_77@hotmail.com>
2017-01-16 18:12:42 +0100

    creation de la fonction session_utilisteur_existe pour vérification de l'existance d'une session

--- function/fonction_session.php
+++ function/fonction_session.php
@@ -8,24 +8,27 @@
 
 
 	/**
-	 *
-	 * 	fonction d'ouverture de la session si celle-ci n'est 
-	 * 	pas deja ouverte
+	 *				session_utilisteur_existe()
+	 *				
+	 * 	fonction de vérification d'ouverture de la session d'un 
+	 * 	utilisateur 
 	 * 	
+	 * 	@return bolean , true si la session est ouverte et FALSE si celle-ci n'existe pas
 	 */
+			function session_utilisateur_existe () {
+				return isset($_SESSION['utilisateur']);
+			}
 	
-
 
 	/**
 	 *
-	 * 	fonction de vérification de l'ouverture de la session
-	 * 	correspondant à un utilisateur précis
+	 * 	fonction pour ouvrir une session et y inserer les données utilisateur
 	 *
-	 *	@param string $id_utilisateur, l'id de l'utilisateur à vérifier
-	 *	@return bolean, vrais si la session est ouverte, faux si celle-ci n'est pas ouverte
-	 *	                ou n'appartien pas à l'utilisateur voulu.
-	 *	                
+	 *
+	 * 
 	 */
+
+
 	
 
 	
